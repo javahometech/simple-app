bf10f98a15a0308a86f52978ecff3952a1873a5d
Hari Kammana <hari.kammana@gmail.com>
2020-02-10 21:44:31 +0530

    Initial setup

---
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -0,0 +1,22 @@
+package in.javahome.myweb.controller;
+/*
+ * 
+ */
+public class Calculator {
+	
+	/*
+	 * @param i
+	 * @param j
+	 * @return int
+	 */
+	public int add(int i, int j){
+		return i+j;
+	}
+	public int multiply(int i, int j){
+		return i*j;
+	}
+	
+	public int sub(int i, int j){
+		return i-j;
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+	<h1 style="color: red; font-size: 50px;" align="center">  JavaHome App Morning Batch</h1>
+</body>
+</html>
